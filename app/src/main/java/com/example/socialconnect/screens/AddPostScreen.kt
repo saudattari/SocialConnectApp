f1ae659d52c08194d8cc0cbde77a0c4cf1c12b27
Mohammad Saud <mohammadsaudattari@gmail.com>
2025-01-25 00:36:18 +0500

    changes

--- app/src/main/java/com/example/socialconnect/screens/AddPostScreen.kt
+++ app/src/main/java/com/example/socialconnect/screens/AddPostScreen.kt
@@ -1,7 +1,6 @@
 package com.example.socialconnect.screens
 
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -9,6 +8,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddPhotoAlternate
@@ -46,7 +47,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.data.EmptyGroup.data
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -56,36 +56,58 @@ import com.cloudinary.android.callback.ErrorInfo
 import com.cloudinary.android.callback.UploadCallback
 import com.example.socialconnect.R
 import com.example.socialconnect.ui.theme.clickColor
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
-import com.google.firebase.firestore.firestore
-import okhttp3.internal.format
-import kotlin.contracts.contract
 
 @Preview
 @Composable
 fun AddPostScreen() {
     var postText by remember { mutableStateOf("") }
-    val listImages = mutableListOf(R.drawable.photo)
-    val context  = LocalContext.current
+    val scrollImage = rememberScrollState()
+    val listImages = mutableListOf<String>()
+    val context = LocalContext.current
+    val auth = FirebaseAuth.getInstance()
+    val currentUser = auth.currentUser?.uid
+    var addedButtonSate = false
     var imageUrlState by remember { mutableStateOf("") }
-    val imageLauncher = rememberLauncherForActivityResult(contract  = ActivityResultContracts.GetContent()) {uri: Uri?->
-        uri?.let {
-//            upload image to Cloudinary
-            uploadImageToCloudinary(
-                context =context,
-                fileUri = it,
-                onSuccess = {fileUri->
-                    imageUrlState = fileUri
-                }
-            )
+    val imageLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+            uri?.let {
+                uploadImageToCloudinary(
+                    context = context,
+                    fileUri = it,
+                    onSuccess = { fileUri ->
+                        imageUrlState = fileUri
+                        listImages.add(imageUrlState)
+                        addedButtonSate = true
+                    }
+
+                )
+            }
         }
-    }
     Scaffold(
         floatingActionButton = {
-            FloatingActionButton(onClick = {},Modifier.clip(RoundedCornerShape(50.dp)), containerColor = clickColor, contentColor = Color.White) {
+            FloatingActionButton(
+
+                onClick = {
+                    currentUser?.let {
+                        uploadContentToFireStore(
+                            context = context,
+                            currentUser = it,
+                            content = postText,
+                            imageUrl = listImages,
+                            onSuccess = {
+                                Toast.makeText(context, "Post published", Toast.LENGTH_SHORT).show()
+                                postText = ""
+                                listImages.clear()
+                            },
+                        )
+                    }
+                },
+                Modifier.clip(RoundedCornerShape(50.dp)),
+                containerColor = clickColor,
+                contentColor = Color.White,
+            ) {
                 Icon(imageVector = Icons.Default.Check, contentDescription = "Add Post")
             }
         }
@@ -115,14 +137,18 @@ fun AddPostScreen() {
                     )
                 }
 //                horizontal Line
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .height(1.dp)
-                    .background(Color.LightGray)) { }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(1.dp)
+                        .background(Color.LightGray)
+                ) { }
 //                Col for adding data
-                Column(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp)) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp)
+                ) {
                     Row(
                         modifier = Modifier.fillMaxWidth(),
                         verticalAlignment = Alignment.CenterVertically
@@ -136,7 +162,9 @@ fun AddPostScreen() {
                         )
                         Spacer(modifier = Modifier.width(8.dp))
                         Text(
-                            text = "mohammadsaud_attari", fontWeight = FontWeight.Bold, fontSize = 16.sp
+                            text = "mohammadsaud_attari",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
                         )
                     }
 
@@ -144,7 +172,7 @@ fun AddPostScreen() {
                     OutlinedTextField(
                         value = postText,
                         placeholder = { Text(text = "What's New") },
-                        onValueChange = {postText = it},
+                        onValueChange = { postText = it },
                         modifier = Modifier
                             .fillMaxWidth()
                             .height(200.dp),
@@ -152,16 +180,22 @@ fun AddPostScreen() {
                     )
                     Spacer(modifier = Modifier.height(8.dp))
 
-                    if(listImages.isNotEmpty()){
-                        ForEach()
-                        listImages.forEach{ imageUrl->
-                            Row(modifier = Modifier
-                                .fillMaxWidth()
-                                .height(80.dp)) {
-                                Image(painter = , contentDescription = "PotContentPicture")
-                            }
+                    if (listImages.isNotEmpty()) {
 
+                        for(url in listImages){
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .horizontalScroll(scrollImage)
+                                    .height(80.dp)
+                            ) {
+                                Image(
+                                    painter = rememberAsyncImagePainter(url),
+                                    contentDescription = "PotContentPicture"
+                                )
+                            }
                         }
+
                     }
 
 //                    add Action buttons
@@ -173,20 +207,48 @@ fun AddPostScreen() {
                             .background(Color.LightGray),
                         verticalAlignment = Alignment.CenterVertically,
                     ) {
-                      IconButton(onClick = {
-                          imageLauncher.launch("image/* , video/*")
-                      }, modifier = Modifier,) { Icon(imageVector = Icons.Default.AddPhotoAlternate, contentDescription = "Add Image", tint = Color.Black) }
-                      IconButton(onClick = {}) { Icon(imageVector = Icons.Default.CameraAlt, contentDescription = "Open Camera", tint = Color.Black) }
-                      IconButton(onClick = {}) { Icon(imageVector = Icons.Default.Tag, contentDescription = "Add Hashtag", tint = Color.Black) }
-                      IconButton(onClick = {}) { Icon(imageVector = Icons.Default.LocationOn, contentDescription = "Add Location", tint = Color.Black) }
-                  }
+                        IconButton(
+                            onClick = {
+                                imageLauncher.launch("image/*")
+                            },
+                            modifier = Modifier,
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.AddPhotoAlternate,
+                                contentDescription = "Add Image",
+                                tint = Color.Black
+                            )
+                        }
+                        IconButton(onClick = {}) {
+                            Icon(
+                                imageVector = Icons.Default.CameraAlt,
+                                contentDescription = "Open Camera",
+                                tint = Color.Black
+                            )
+                        }
+                        IconButton(onClick = {}) {
+                            Icon(
+                                imageVector = Icons.Default.Tag,
+                                contentDescription = "Add Hashtag",
+                                tint = Color.Black
+                            )
+                        }
+                        IconButton(onClick = {}) {
+                            Icon(
+                                imageVector = Icons.Default.LocationOn,
+                                contentDescription = "Add Location",
+                                tint = Color.Black
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
-fun uploadImageToCloudinary(fileUri: Uri, onSuccess:(String) ->Unit, context:Context){
-    MediaManager.get().upload(fileUri).callback(object :  UploadCallback{
+
+fun uploadImageToCloudinary(fileUri: Uri, onSuccess: (String) -> Unit, context: Context) {
+    MediaManager.get().upload(fileUri).callback(object : UploadCallback {
         override fun onStart(requestId: String?) {
             Toast.makeText(context, "Upload started", Toast.LENGTH_SHORT).show()
 
@@ -194,11 +256,12 @@ fun uploadImageToCloudinary(fileUri: Uri, onSuccess:(String) ->Unit, context:Con
 
         override fun onProgress(requestId: String?, bytes: Long, totalBytes: Long) {
             val totalTime = (bytes.toDouble() / totalBytes) * 100
-            Toast.makeText(context, "Uploading started...${totalTime.toInt()}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Uploading started...${totalTime.toInt()}", Toast.LENGTH_SHORT)
+                .show()
         }
 
         override fun onSuccess(requestId: String?, resultData: MutableMap<Any?, Any?>?) {
-            val fileUri =resultData?.get("url") as String
+            val fileUri = resultData?.get("url") as String
             Toast.makeText(context, "Uploading completed", Toast.LENGTH_SHORT).show()
             onSuccess(fileUri)
         }
@@ -212,18 +275,29 @@ fun uploadImageToCloudinary(fileUri: Uri, onSuccess:(String) ->Unit, context:Con
 
     })
 }
- fun uploadContentToFireStore(context: Context,currentUser:String,content:String, imageUrl:String,onSuccess: () -> Unit){
-     val db = FirebaseFirestore.getInstance()
-     val postDocRef = db.collection("posts").document(currentUser)
-     val data  = hashMapOf(
-         "content" to content,
-         "ContentImage" to imageUrl,
-         "userId" to currentUser
-     )
-     postDocRef.set(data, SetOptions.merge()).addOnSuccessListener {
-         onSuccess()
-     }
-         .addOnFailureListener {
-             Toast.makeText(context, "Sorry! getting error while uploading post on FireStore", Toast.LENGTH_SHORT).show()
-         }
- }
+
+fun uploadContentToFireStore(
+    context: Context,
+    currentUser: String,
+    content: String,
+    imageUrl:List<String>,
+    onSuccess: () -> Unit
+) {
+    val db = FirebaseFirestore.getInstance()
+    val postDocRef = db.collection("posts").document(currentUser)
+    val data = hashMapOf(
+        "content" to content,
+        "ContentImage" to imageUrl,
+        "userId" to currentUser
+    )
+    postDocRef.set(data, SetOptions.merge()).addOnSuccessListener {
+        onSuccess()
+    }
+        .addOnFailureListener {
+            Toast.makeText(
+                context,
+                "Sorry! getting error while uploading post on FireStore",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+}
